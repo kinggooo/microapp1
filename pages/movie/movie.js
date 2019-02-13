b4eae9dc986183f2d8dd09272ec4f8efd3d60e5f
wangnz <wangnz@qq.com>
2019-02-13 19:58:45 +0800

    upd
    upd

--- pages/movie/movie.js
+++ pages/movie/movie.js
@@ -1,11 +1,47 @@
 Page({
   data: {
-    inputValue: ''
+    players: {
+      "p1":{
+        name: 'CHN',
+        id: '1',
+        score: '0',
+        checked: 'true'
+      },
+      "p2":{
+        name: 'BRA',
+        id: '2',
+        score: '0'
+      },
+      "p3":{
+        name: 'JPN',
+        id: '3',
+        score: '0'
+      },
+      "p4":{
+        name: 'ENG',
+        id: '4',
+        score: '0'
+      }
+    },
+    inputValue: '',
+    score: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;score&nbsp;:&nbsp;"
   },
   bindKeyInput(e) {
-    console.log(e.id)
-    this.setData({
-      inputValue: e.target.id
-    })
+    
+    // this.setData({
+    //   inputValue: e.target.id
+    // })
+    this.data.players[e.target.id].name = e.detail.value
+    console.log(this.data.players[e.target.id].name)
+  },
+  zimo(e) {
+
+  },
+  zhuachong(e) {
+
+  },
+  radioChange(e) {
+    var name = this.data.players[e.detail.value].name
+    console.log('radio发生change事件，携带value值为：', name)
   }
-}) 
+})
