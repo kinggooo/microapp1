0e97a3438c5fd36546e08193649ab91ea418611c
wangnz <wangnz@qq.com>
2019-02-15 18:00:06 +0800

    upd

--- pages/movie/movie.js
+++ pages/movie/movie.js
@@ -1,47 +1,60 @@
 Page({
   data: {
     players: {
-      "p1":{
+      "p1": {
         name: 'CHN',
         id: '1',
         score: '0',
         checked: 'true'
       },
-      "p2":{
+      "p2": {
         name: 'BRA',
         id: '2',
         score: '0'
       },
-      "p3":{
+      "p3": {
         name: 'JPN',
         id: '3',
         score: '0'
       },
-      "p4":{
+      "p4": {
         name: 'ENG',
         id: '4',
         score: '0'
       }
     },
     inputValue: '',
+    amt: '0',
+    selectedPlayer: 'p1',
     score: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;score&nbsp;:&nbsp;"
   },
   bindKeyInput(e) {
-    
-    // this.setData({
-    //   inputValue: e.target.id
-    // })
+    var obj = {}
+    obj[e.target.id] = e.detail.value
+    this.setData(obj)
+  },
+  bindInputPname(e) {
     this.data.players[e.target.id].name = e.detail.value
-    console.log(this.data.players[e.target.id].name)
   },
   zimo(e) {
-
+    var amt = this.data.amt
+    var players = this.data.players
+    for (var key in players) {
+      if (key == this.data.selectedPlayer){
+        console.log(players[key].name)
+        console.log(players[key].score)
+        players[key].score = Number(players[key].score) + Number(this.data.amt)
+      }
+    }
   },
   zhuachong(e) {
 
   },
   radioChange(e) {
-    var name = this.data.players[e.detail.value].name
-    console.log('radio发生change事件，携带value值为：', name)
+    // var name = this.data.players[e.detail.value].name
+    // console.log('radio发生change事件，携带value值为：', name)
+    this.setData({
+      selectedPlayer: e.detail.value
+    })
   }
 })
