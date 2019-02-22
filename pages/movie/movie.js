ef1fa02fce7d7521abd3cd24fd299f02ccafcb56
wangnz <wangnz@qq.com>
2019-02-22 11:19:55 +0800

    upd

--- pages/movie/movie.js
+++ pages/movie/movie.js
@@ -3,8 +3,7 @@ Page({
     players: [{
         name: 'CHN',
         id: 'p1',
-        score: '0',
-        checked: 'true'
+        score: '0'
       },
       {
         name: 'BRA',
@@ -22,34 +21,54 @@ Page({
         score: '0'
       }
     ],
+    idToIdx: {
+      'p1': 0,
+      'p2': 1,
+      'p3': 2,
+      'p4': 3
+    },
     inputValue: '',
     amt: '0',
-    selectedPlayer: '0',
-    score: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;score&nbsp;:&nbsp;"
+    score: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;score&nbsp;:&nbsp;",
+    winnerIdx: 0,
+    loserIdx: 0
   },
   bindKeyInput(e) {
     var obj = {}
     obj[e.target.id] = e.detail.value
     this.setData(obj)
   },
-  bindInputPname(e) {
+  bindPnameInput(e) {
     // this.data.players[e.target.id].name = e.detail.value
-    var player = this.data.players[this.data.selectedPlayer]
+    var idx = this.data.idToIdx[e.target.id]
+    var player = this.data.players[idx]
     player.name = e.detail.value
     this.setData({
       players: this.data.players
     })
   },
   zimo(e) {
+    var selPlayer = this.data.winnerIdx
     var amt = this.data.amt
+    // 校验数据
+    if (amt == null || amt == 0) {
+      wx.showToast({
+        title: '请先输入金额！',
+        icon: 'none',
+        duration: 2000
+      })
+    }
+
     var players = this.data.players
     for (var key in players) {
-      if (key == this.data.selectedPlayer) {
-        players[key].score = Number(players[key].score) + Number(this.data.amt) * 3
+      if (key == this.data.winnerIdx) {
+        players[key].score = Number(players[key].score) + Number(amt) * 3
       } else {
-        players[key].score = Number(players[key].score) - Number(this.data.amt)
+        players[key].score = Number(players[key].score) - Number(amt)
       }
     }
+
+    // 刷新视图数据
     this.setData({
       players: this.data.players
     })
@@ -57,9 +76,12 @@ Page({
   zhuachong(e) {
     var amt = this.data.amt
     var players = this.data.players
-    for (var key in players) {
-      if (key == this.data.selectedPlayer) {
-        players[key].score = Number(players[key].score) + Number(this.data.amt)
+    for (var idx in players) {
+      if (idx == this.data.winnerIdx) {
+        players[idx].score = Number(players[idx].score) + Number(amt)
+      }
+      if (idx == this.data.loserIdx) {
+        players[idx].score = Number(players[idx].score) - Number(amt)
       }
     }
     this.setData({
@@ -71,7 +93,12 @@ Page({
     // console.log('radio发生change事件，携带value值为：', name)
     e.target.checked = true
     this.setData({
-      selectedPlayer: e.detail.value
+      winnerIdx: e.detail.value
+    })
+  },
+  bindPickerChange(e) {
+    this.setData({
+      loserIdx: e.detail.value
     })
   }
 })
